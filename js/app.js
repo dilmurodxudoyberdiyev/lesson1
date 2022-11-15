5362128ed9086b4917000dfb3c7e1e492e1cd011
dilmurodxudoyberdiyev <dxudoyberdiyev041@gmail.com>
2022-11-15 21:26:16 +0500

    lesson1

---
+++ js/app.js
@@ -0,0 +1,48 @@
+// var userName = prompt("Ismingizni kiriting");
+// var age = prompt("Yoshingiz nechida");
+// var workPlace = prompt("Ish joyingizni kiriting");   
+// var interest = prompt("Nimaga qiziqasiz");
+
+// console.log(`Ismingizni kiriting ${userName}. Yoshim ${age} da. Hozirda  ${workPlace} da o'qiyman. bo'sh vaqtimda ${interest} o'qiyman.`);
+
+
+// 1 misol
+
+
+// var a = 10;
+// var b = 5;
+
+// console.log(a+b);
+
+// 2 misol
+
+// var a = 25;
+// var b = 15;
+
+// result = a + b;
+
+// console.log(result);
+
+
+// 3 misol
+
+
+// var a = 10;
+// var b = 5;
+
+// c = a - b;
+
+// console.log(c);
+
+// 4 misol 
+
+// var a = 10;
+// var b = 3;
+
+
+// console.log(a % b);
+
+// 5 misol
+
+// console.log(2**10);
+
